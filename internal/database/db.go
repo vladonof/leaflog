0276fd9f4ec8c02aa7f05e3ed3114752f8398176
Vlad Onofrei <vlad.onofrei@feast-it.com>
2024-08-31 17:38:22 +0100

    chore: add db entities and migrations

--- internal/database/db.go
+++ internal/database/db.go
@@ -8,18 +8,14 @@ import (
 	"gorm.io/gorm"
 )
 
-// DB is a global variable that will hold the connection pool
 var DB *gorm.DB
 
-// ConnectDatabase initializes the database connection
 func ConnectDatabase() {
-	// Get the database connection string from an environment variable
 	dsn := os.Getenv("DATABASE_DSN")
 	if dsn == "" {
 		log.Fatal("DATABASE_DSN environment variable not set")
 	}
 
-	// Open a connection to the database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database: ", err)
